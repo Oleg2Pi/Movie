b9435da68c724f6d624907dfc05cae7d832dbb18
Oleg2Pi <polikarpovoleg115@gmail.com>
2024-05-05 21:56:20 +0300

    Add static and templates

--- django_movie/django_movie/settings.py
+++ django_movie/django_movie/settings.py
@@ -108,6 +108,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -116,3 +118,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
